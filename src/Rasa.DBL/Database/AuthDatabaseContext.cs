602dd80ef1968f49e9c386645f4466c6f3019c55
irdev <47341685+infiniterasadev@users.noreply.github.com>
2019-06-15 21:49:44 -0400

    Attempts to update everything to netcore 3.0

---
+++ src/Rasa.DBL/Database/AuthDatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace Rasa.DBL.Database
+{
+    using Tables.Auth;
+
+    internal class AuthDatabaseContext : DbContext
+    {
+        readonly DbConnection connection;
+
+        public DbSet<Account> Accounts { get; set; }
+
+        public AuthDatabaseContext(DbConnection connection)
+        {
+            this.connection = connection;            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(connection);
+        }
+    }
+}
---
+++ src/Rasa.DBL/Database/Tables/Auth/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Rasa.DBL.Database.Tables.Auth
+{
+    internal class Account
+    {
+        public uint AccountId { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+        public byte Level { get; set; }
+        public byte LastServerId { get; set; }
+        public bool Locked { get; set; }
+        public bool Validated { get; set; }
+
+        public void HashPassword()
+        {
+            Password = Hash(Password, Salt);
+        }
+
+        public bool CheckPassword(string password)
+        {
+            return Hash(password, Salt) == Password;
+        }
+
+        public static string Hash(string password, string salt)
+        {
+            using (var sha = SHA256.Create())
+                return BitConverter.ToString(sha.ComputeHash(Encoding.UTF8.GetBytes($"{salt}:{password}"))).Replace("-", "").ToLower();
+        }
+    }
+}
